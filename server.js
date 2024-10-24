ef3ce26223489f822041c0c5f2ae2e091b748cd6
Shauryaveer Singh <1100050281@edu.cbe.ab.ca>
2024-10-24 13:47:02 -0600

    passing 21 tests

--- server.js
+++ server.js
@@ -1,32 +1,124 @@
 const express = require("express");
-const uuid = require("uuid");
+const uuid = require("uuid")
 const server = express();
-server.use(express.json());
+server.use(express.json())
+server.use(express.static('public'))
 
-let activeSessions = {};
-
-// Utility function to get a random word (placeholder logic for now)
-const getRandomWord = () => {
-    const words = ["apple", "phase", "grape", "plane", "bread"];
-    return words[Math.floor(Math.random() * words.length)];
-};
-
-// Helper to initialize a new game state
-const initializeGameState = (wordToGuess) => {
-    const word = wordToGuess || getRandomWord();
-    return {
-        wordToGuess: word,
-        guesses: [],
+let activeSessions={}
+server.get("/newgame", (req,res)=>{
+    let setWord = req.query.answer
+    let generated="apple"
+    let over=false
+    if(!setWord||setWord.length!=5){
+        setWord=generated
+    }
+    let newID = uuid.v4()
+    let newGame = {
+        wordToGuess: setWord,
+        guesses:[],
         wrongLetters: [],
         closeLetters: [],
         rightLetters: [],
         remainingGuesses: 6,
-        gameOver: false
-    };
-};
+        gameOver: over,
+    }
+    activeSessions[newID] = newGame
+    res.status(201)
+    res.send({sessionID: newID})
+})
 
 
+server.get("/gamestate", (req,res)=>{
+    let ID= req.query.sessionID
+    if(!ID){
+        res.status(400)
+        res.send({error: "id is missing"})
+    }else{
+        if(activeSessions[ID]){
+            res.status(200)
+            res.send({gameState: activeSessions[ID]})
+        }else{
+            res.status(404)
+            res.send({error: "game doesn't exist"})
+        }
+    }
+})
 
-// Do not remove this line. This allows the test suite to start
-// multiple instances of your server on different ports
+server.post("/guess", (req, res) => {
+    let sessionID = req.body.sessionID;
+    let userGuess = req.body.guess;
+
+    if (!sessionID) {
+        return res.status(400).json({ error: "Session ID is missing" });
+    }
+
+    if (activeSessions[sessionID]) {
+        let game = activeSessions[sessionID];
+        let guessLetters = userGuess.split("");
+
+        if (guessLetters.length !== 5) {
+            return res.status(400).json({ error: "Guess must be exactly 5 letters long" });
+        }
+
+        let wordToGuess = game.wordToGuess.split("");
+        let guessResults = [];
+        game.remainingGuesses--;
+
+        if (userGuess === game.wordToGuess) {
+            game.gameOver = true;
+            game.rightLetters = guessLetters;
+        } else {
+            for (let i = 0; i < guessLetters.length; i++) {
+                let letter = guessLetters[i].toLowerCase();
+
+                if (!letter.match(/[a-z]/)) {
+                    return res.status(400).json({ error: "Guess contains invalid characters" });
+                }
+
+                let guessDetail = { value: letter };
+                let found = false;
+
+                for (let j = 0; j < wordToGuess.length; j++) {
+                    if (wordToGuess[j] === letter) {
+                        if (i === j) {
+                            if (game.closeLetters.includes(letter)) {
+                                let index = game.closeLetters.indexOf(letter);
+                                game.closeLetters.splice(index, 1);
+                            }
+                            game.rightLetters.push(letter);
+                            guessDetail.result = "RIGHT";
+                        } else {
+                            if (!game.closeLetters.includes(letter)) {
+                                game.closeLetters.push(letter);
+                            }
+                            guessDetail.result = "CLOSE";
+                        }
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
+                    game.wrongLetters.push(letter);
+                    guessDetail.result = "WRONG";
+                }
+
+                guessResults.push(guessDetail);
+            }
+        }
+
+        if (game.remainingGuesses === 0) {
+            game.gameOver = true;
+        }
+
+        game.guesses.push(guessResults);
+        return res.status(201).json({ gameState: game });
+    } else {
+        return res.status(404).json({ error: "Game does not exist" });
+    }
+});
+
+
+//Do not remove this line. This allows the test suite to start
+//multiple instances of your server on different ports
 module.exports = server;
